6c78bfe96c53a096729bd29fd5ddcd4387fa61cb
hitochan151 <fbx3jpef6@gmail.com>
2020-11-25 00:18:46 +0900

    usersテーブルに追加するカラムを記述

--- db/migrate/20201124151809_devise_create_userrails.rb
+++ db/migrate/20201124151809_devise_create_userrails.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+class DeviseCreateUserrails < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :userrails do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname,           null: false
-      t.string :family_name,        null: false
-      t.string :frist_name,         null: false
-      t.string :family_name_kana,   null: false
-      t.string :frist_name_kana,    null: false
-      t.date   :brith_day,          null: false
-      
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -38,13 +32,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.migrate :db
 
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :userrails, :email,                unique: true
+    add_index :userrails, :reset_password_token, unique: true
+    # add_index :userrails, :confirmation_token,   unique: true
+    # add_index :userrails, :unlock_token,         unique: true
   end
 end
