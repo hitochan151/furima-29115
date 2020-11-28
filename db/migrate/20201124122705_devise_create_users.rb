935f31f427236cfec4ea98dfb6ba874173b82622
hitochan151 <fbx3jpef6@gmail.com>
2020-11-28 15:39:05 +0900

    userテーブルカラム名修正

--- db/migrate/20201124122705_devise_create_users.rb
+++ db/migrate/20201124122705_devise_create_users.rb
@@ -8,10 +8,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname,           null: false
       t.string :family_name,        null: false
-      t.string :frist_name,         null: false
+      t.string :first_name,         null: false
       t.string :family_name_kana,   null: false
-      t.string :frist_name_kana,    null: false
-      t.date   :brith_day,          null: false
+      t.string :first_name_kana,    null: false
+      t.date   :birth_day,          null: false
       
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,10 @@ ActiveRecord::Schema.define(version: 2020_11_24_122705) do
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
     t.string "family_name", null: false
-    t.string "frist_name", null: false
+    t.string "first_name", null: false
     t.string "family_name_kana", null: false
-    t.string "frist_name_kana", null: false
-    t.date "brith_day", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
