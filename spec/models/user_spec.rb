12bc50176cac14c73da58b4b893c4b77a96cc439
hitochan151 <fbx3jpef6@gmail.com>
2020-11-30 15:35:37 +0900

    exampleを整理

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,36 @@
 require 'rails_helper'
- RSpec.describe User, type: :model do
+describe User do
   before do
     @user = FactoryBot.build(:user)
   end
 
-   describe 'ユーザー新規登録' do
-     it "nicknameが空だと登録できない" do
-      @user.nickname = ""
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+      end
+      it "passwordは半角英数字混合であれば登録できる" do
+      end
+      it "passwordが6文字以上であれば登録できる" do
+      end
     end
-    it "emailが空では登録できない" do
-      @user.email = ""
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Email can't be blank"
+
+    context '新規登録がうまくいかないとき' do
+      it "nicknameが空だと登録できない" do
+      end
+      it "email@が空では登録できない" do
+      end
+      it "emailが空では登録できない" do
+      end
+      it "重複したemailが存在する場合登録できない" do
+      end
+      it "passwordが空では登録できない" do
+      end
+      it "passwordが5文字以下であれば登録できない" do
+      end
+      it "passwordが存在してもpassword_confirmationが空では登録できない" do
+      end
+      it "passwordは半角英数字混合以外では登録できない" do
+      end
     end
-   end
- end
+  end
+end
