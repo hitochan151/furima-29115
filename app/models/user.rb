faf77be5cd1b2df84a337a3481334220a92cf671
hitochan151 <fbx3jpef6@gmail.com>
2020-11-30 18:49:48 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :nickname presence: true
-    validates :email presence: true
-    validates :family_name presence: true
-    validates :first_name presence: true
-    validates :family_name_kana presence: true
-    validates :first_name_kana presence: true
-    validates :birth_day presence: true
-    validates :encrypted_password presence: true
+    validates :nickname, presence: true
+    validates :birth_day, presence: true
+    validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    
+    with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+     validates :family_name
+     validates :first_name
+    end
+
+    with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } do
+      validates :family_name_kana
+      validates :first_name_kana
+     end
       
   end
