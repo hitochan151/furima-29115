0255dfacea1eb3af40dd721fb5797fdef243818b
hitochan151 <fbx3jpef6@gmail.com>
2020-11-30 20:35:16 +0900

    FactoryBot記述修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,11 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    
+    birth_day             {"1990-10-29"}
+    family_name           {"山田"}
+    first_name            {"太郎"}
+    family_name_kana      {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ describe User do
       end
       it "passwordは半角英数字混合であれば登録できる" do
         @user.password = "aaaa1111"
+        @user.password_confirmation = "aaaa1111"
         expect(@user).to be_valid
       end
       it "passwordが6文字以上であれば登録できる" do
