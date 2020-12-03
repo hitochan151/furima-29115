fd72955759186379aceefcdbf6c81c8318fe507f
hitochan151 <fbx3jpef6@gmail.com>
2020-12-03 18:11:04 +0900

    画像の保存を許可するストロングパラメーター定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,10 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
