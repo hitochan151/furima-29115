477a95e5d44482a10b5e7eef66c840dff5eeb5b6
hitochan151 <fbx3jpef6@gmail.com>
2020-12-03 22:23:33 +0900

    itemモデルのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :product_status_id
+  belongs_to :shopping_charges_id
+  belongs_to :prefecture_id
+  belongs_to :days_id
 end
