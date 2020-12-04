7ccb86773d107a599d139edcb45442b518ec3de0
hitochan151 <fbx3jpef6@gmail.com>
2020-12-04 21:02:47 +0900

    itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture_id
   belongs_to :days_id
 
+  validates :name, :description, :category_id, :product_status_id, 
+  :prefecture_id, :days_id, presence: true
+  
+  validates :selling_price, presence: true, format: {with: /\A[0-9]+\z/ } 
+  validates :selling_price, numericality: { greater_than_or_equal_to: 300 } 
+  validates :selling_price, numericality: { less_than_or_equal_to: 9,999,999 } 
+
+
 end
