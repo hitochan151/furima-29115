4f9b6232b8d332faf1614b613190dee9aa7d047b
hitochan151 <fbx3jpef6@gmail.com>
2020-12-06 18:16:59 +0900

    itemコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.valid?
+       @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def message_params
+  def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
 
