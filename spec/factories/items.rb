8943d99e9cef2b59344c76a54723f9f50b3fc80e
hitochan151 <fbx3jpef6@gmail.com>
2020-12-08 20:14:21 +0900

    FactoryBotコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name                 {"商品"}
-    description          {"good"}}
-    category_id          {1}
-    product_status_id    {1}
-    prefecture_id        {1}
-    days_id              {1}
-    selling_price        {1}
-    shopping_charges_id  {1}
+    name                 { "商品" }
+    description          { "good" }
+    category_id          { 1 }
+    product_status_id    { 1 }
+    prefecture_id        { 1 }
+    days_id              { 1 }
+    selling_price        { 1 }
+    shopping_charges_id  { 1 }
 
 
       association :user
