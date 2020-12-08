fa78343aaeb6bea5cdce7d6ed2d5d7108018ed1e
hitochan151 <fbx3jpef6@gmail.com>
2020-12-08 20:04:22 +0900

    FactoryBot記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
   factory :item do
-      content {Faker::Lorem.sentence}
+    name                 {"商品"}
+    description          {"good"}}
+    category_id          {1}
+    product_status_id    {1}
+    prefecture_id        {1}
+    days_id              {1}
+    selling_price        {1}
+    shopping_charges_id  {1}
+
+
       association :user
       association :purchaser
 
