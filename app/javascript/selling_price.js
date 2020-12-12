81ae78fd174011b5bcac3d669875f62fb62a7bb5
hitochan151 <fbx3jpef6@gmail.com>
2020-12-12 14:43:50 +0900

    販売手数料、販売利益の表示

--- app/javascript/selling_price.js
+++ app/javascript/selling_price.js
@@ -1,5 +1,15 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+    console.log(addTaxDom);
+
+    const profitNumber = document.getElementById("profit");
+    profitNumber.innerHTML = (Math.floor(inputValue * 0.9));
+    console.log(profitNumber);
+  })
 });
-const priceInput = document.getElementById("金額を入力する場所のid");
-console.log(priceInput);
