362ce7c5ae81c95a15a6aeb23e7553f6ba977531
hitochan151 <fbx3jpef6@gmail.com>
2020-12-27 16:46:19 +0900

    コントローラーFormオブジェクト、インスタンス記述

--- app/controllers/purchaser_destniation_controller.rb
+++ app/controllers/purchaser_destniation_controller.rb
@@ -1,2 +1,22 @@
 class PurchaserDestniationController < ApplicationController
+
+  def index
+    @purchaser_destniation = Purchaserdestniation.new
+  end
+
+  def create
+    @purchaser_destniation = Purchaserdestniation.new(purchaser_params)
+    if @purchaser_destniation.valid?
+      @purchaser_destniation.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def purchaser_params
+    params.require(:order).permit(:price)
+  end
 end
