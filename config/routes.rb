ddf7a2e2ff37625531c445c5076e6c22f0ab0577
hitochan151 <fbx3jpef6@gmail.com>
2020-12-27 17:28:34 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items do
-    resources :purchaser_destniation, only: :create :index
+    resources :purchaser_destniation, only: [:create, :index]
   end
 end
