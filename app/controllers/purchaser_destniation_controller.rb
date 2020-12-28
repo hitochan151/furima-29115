5002fae00de38c2eb5c4c6b3f1b573c2445624af
hitochan151 <fbx3jpef6@gmail.com>
2020-12-28 17:35:34 +0900

    コントローラー修正

--- app/controllers/purchaser_destniation_controller.rb
+++ app/controllers/purchaser_destniation_controller.rb
@@ -1,23 +1,23 @@
 class PurchaserDestniationController < ApplicationController
 
   def index
-    @purchaser_destniation = PurchaserDestniation.new
+    @purchaser_destniation = PurchaserDestniation.new(params[:item_id])
   end
 
   def create
-   
-    @purchaser_destniation = PurchaserDestniation.new(purchaser_params)
+   @purchaser_destniation = PurchaserDestniation.new(purchaser_destniation_params)
     if @purchaser_destniation.valid?
       @purchaser_destniation.save
       return redirect_to root_path
     else
-      render 'index'
+      render action: :index
     end
   end
 
   private
 
-  def purchaser_params
-    params.require(:purchaser_destniation).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchaser_id, :item_id, :user_id)
+  def purchaser_destniation_params
+    params.require(:purchaser_destniation).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchaser_id)
+    .merge(item_id: params[:item_id])
   end
 end
--- app/models/purchaser_destniation.rb
+++ app/models/purchaser_destniation.rb
@@ -1,12 +1,11 @@
 class PurchaserDestniation
   include ActiveModel::Model
-  attr_accessor :user, :item, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchaser 
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchaser 
 
   with_options presence: true do
     validates :city, :address, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
     validates :phone_number, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters." }
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   end
   
