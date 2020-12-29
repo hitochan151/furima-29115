812cd11f29a9de9e554d2cf30c4688962299c5e5
hitochan151 <fbx3jpef6@gmail.com>
2020-12-29 20:41:40 +0900

    jsファイルname属性記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,15 +3,15 @@ const pay = () => {
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    
+
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      number: formData.get("purchaser_destniation[number]"),
+      cvc: formData.get("purchaser_destniation[cvc]"),
+      exp_month: formData.get("purchaser_destniation[exp_month]"),
+      exp_year: `20${formData.get("purchaser_destniation[exp_year]")}`,
     };
   });
 };
