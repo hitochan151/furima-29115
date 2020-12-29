0a9edadd99207b4cedf2de7c7e3b37383a997a53
hitochan151 <fbx3jpef6@gmail.com>
2020-12-29 12:47:44 +0900

    コントローラーuser_id追加

--- app/controllers/purchaser_destniation_controller.rb
+++ app/controllers/purchaser_destniation_controller.rb
@@ -1,7 +1,7 @@
 class PurchaserDestniationController < ApplicationController
 
   def index
-    @purchaser_destniation = PurchaserDestniation.new(params[:item_id])
+    @purchaser_destniation = PurchaserDestniation.new
   end
 
   def create
@@ -18,6 +18,6 @@ class PurchaserDestniationController < ApplicationController
 
   def purchaser_destniation_params
     params.require(:purchaser_destniation).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchaser_id)
-    .merge(item_id: params[:item_id])
+    .merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
