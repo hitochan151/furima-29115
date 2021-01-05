489c5ae1ca9ddec1ea051db2babbb975b1faca5f
hitochan151 <fbx3jpef6@gmail.com>
2021-01-05 21:21:28 +0900

    validates「phone_number」修正

--- app/models/purchaser_destniation.rb
+++ app/models/purchaser_destniation.rb
@@ -4,7 +4,7 @@ class PurchaserDestniation
 
   with_options presence: true do
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :phone_number, format: { with: /\A[0-9]+\z/}
+    validates :phone_number, format: { with: /\A\d{11}\z/}
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id, numericality: { other_than: 1}
     validates :token, :address, presence: true
