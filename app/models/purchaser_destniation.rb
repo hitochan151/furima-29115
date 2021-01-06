a59d73b6fa2b2aa6dbbd82c4181002cc63f09657
hitochan151 <fbx3jpef6@gmail.com>
2021-01-06 18:44:03 +0900

    phone_numberのvalidatesテストコード 修正

--- app/models/purchaser_destniation.rb
+++ app/models/purchaser_destniation.rb
@@ -3,10 +3,10 @@ class PurchaserDestniation
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchaser, :token
 
   with_options presence: true do
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :phone_number, format: { with: /\A\d{10,11}\z/}
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
-    validates :prefecture_id, numericality: { other_than: 1}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :phone_number, numericality: { only_interger: /\A\d{10,11}\z/ }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1} 
     validates :token, :address, :user_id, :item_id, presence: true
   end
 
--- spec/models/purchaser_destniation_spec.rb
+++ spec/models/purchaser_destniation_spec.rb
@@ -8,71 +8,71 @@ RSpec.describe PurchaserDestniation, type: :model do
       @item = FactoryBot.build(:item)
     end
 
-    it 'すべての値が正しく入力されていれば保存できること' do
-       @purchaser_destniation.errors.full_messages
-      expect(@purchaser_destniation).to be_valid
+    context '商品購入ができる時' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+         @purchaser_destniation.errors.full_messages
+        expect(@purchaser_destniation).to be_valid
+       end
+      it 'building_nameは空でも保存できること' do
+        @purchaser_destniation.building_name = ' '
+        expect(@purchaser_destniation).to be_valid
+      end
     end
-    it 'post_codeが空だと保存できないこと' do
-      @purchaser_destniation.post_code = nil
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("Post code can't be blank", "Post code is invalid")
-    end
-    it 'post_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
-      @purchaser_destniation.post_code = '1234567'
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("Post code is invalid")
-    end
-    it 'prefecture_idを選択していないと保存できないこと' do
-      @purchaser_destniation.prefecture_id = 1
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("Prefecture must be other than 1")
-    end
-    it 'cityを選択していないと保存できないこと' do
-      @purchaser_destniation.city = nil
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("City can't be blank", "City is invalid")
-    end
-    it 'cityが半角数字だと保存できないこと' do
-      @purchaser_destniation.city = '1234'
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("City is invalid")
-    end
-    it 'addressを選択していないと保存できないこと' do
-      @purchaser_destniation.address = nil
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("Address can't be blank")
-    end
-    it 'building_nameは空でも保存できること' do
-      @purchaser_destniation.building_name = ' '
-      expect(@purchaser_destniation).to be_valid
-    end
-    it 'phone_numberを選択していないと保存できないこと' do
-      @purchaser_destniation.phone_number = nil
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("Phone number can't be blank")
-    end
-    it 'phone_numberがハイフンを含まない半角の11文字以内でないと保存できないこと' do
-      @purchaser_destniation.post_code = '１２３ー４５６７ー１２３４５'
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("Post code is invalid")
-    end
-    it "priceとtokenがあれば保存ができること" do
-      expect(@purchaser_destniation).to be_valid
-    end
-    it "tokenが空では登録できないこと" do
-      @purchaser_destniation.token = nil
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("Token can't be blank")
-    end
-    it "user_idが空では登録できないこと" do
-      @purchaser_destniation.user_id = nil
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("User can't be blank")
-    end
-    it "item_idが空では登録できないこと" do
-      @purchaser_destniation.item_id = nil
-      @purchaser_destniation.valid?
-      expect(@purchaser_destniation.errors.full_messages).to include("Item can't be blank")
+
+    context '商品購入ができない時' do
+      it 'post_codeが空だと保存できないこと' do
+       @purchaser_destniation.post_code = nil
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("Post code can't be blank", "Post code is invalid")
+      end
+      it 'post_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @purchaser_destniation.post_code = '1234567'
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("Post code is invalid")
+      end
+      it 'prefecture_idを選択していないと保存できないこと' do
+        @purchaser_destniation.prefecture_id = 1
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'cityを選択していないと保存できないこと' do
+        @purchaser_destniation.city = nil
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("City can't be blank", "City is invalid")
+      end
+      it 'cityが半角数字だと保存できないこと' do
+        @purchaser_destniation.city = '1234'
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("City is invalid")
+      end
+      it 'addressを選択していないと保存できないこと' do
+        @purchaser_destniation.address = nil
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'phone_numberを選択していないと保存できないこと' do
+        @purchaser_destniation.phone_number = nil
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberがハイフンを含まない半角英数混合の11文字以内でないと保存できないこと' do
+        @purchaser_destniation.post_code = '１２３ー４５６７ー１２３４５'
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("Post code is invalid")
+      end
+      it "priceとtokenがあれば保存ができること" do
+        expect(@purchaser_destniation).to be_valid
+      end
+      it "user_idが空では登録できないこと" do
+        @purchaser_destniation.user_id = nil
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("User can't be blank")
+      end
+      it "item_idが空では登録できないこと" do
+        @purchaser_destniation.item_id = nil
+        @purchaser_destniation.valid?
+        expect(@purchaser_destniation.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
